f6d9d9ac100f0e61824013b0d37e26055f4d03b4
Brian Cassidy <bricas@cpan.org>
2009-11-18 20:19:52 -0400

    prep release with new Module::Install

--- lib/HTML/TagCloud/Sortable.pm
+++ lib/HTML/TagCloud/Sortable.pm
@@ -5,7 +5,7 @@ use warnings;
 
 use base qw( HTML::TagCloud );
 
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 
 =head1 NAME
 
@@ -215,7 +215,7 @@ Brian Cassidy E<lt>bricas@cpan.orgE<gt>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright 2008 by Brian Cassidy
+Copyright 2007-2009 by Brian Cassidy
 
 This library is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself. 
